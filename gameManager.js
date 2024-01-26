dc06293271306861ca0c8c0051ffbfc8a25fa9de
EmilL22r <emilvirkelystlindholm@gmail.com>
2024-01-26 13:49:09 +0200

    Update gameManager.js

--- gameManager.js
+++ gameManager.js
@@ -18,6 +18,37 @@ class GameManager
     // Show score and highscore on screen
     displayScore()
     {
-        
+
     }
-}
+
+    addToScore(amount)
+    {
+        this.score = this.score + amount
+    }
+
+
+        // Use this function to track the increase in bullet damage from power-ups
+        damageIncrease()
+        {
+    
+        }
+
+        // Use this function to track the increase in attack speed from power-ups
+        attackSpeedIncrease()
+        {
+
+        }
+
+        // Use this function to track the increase in how many enemies bullets can penetrate acquired from power-ups
+        bulletPenetrationIncrease()
+        {
+
+        }
+
+        // Use this function to track the increase in bullet capacity between reloads from power-ups
+        ammoCapacityIncrease()
+        {
+
+        }
+
+    }
