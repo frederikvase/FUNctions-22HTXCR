d09330f71c4f14a7b946fdb42b21924d16514b5a
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2024-01-26 13:58:04 +0100

    Update Projectile.js

--- Projectile.js
+++ Projectile.js
@@ -1,8 +1,28 @@
 class Projectile extends Entity
 {
-    constructor()
+    constructor(imageFileName, x, x, xSpeed, ySpeed, xScale, yScale)
     {
-        super()
+        super(imageFileName, x, y, xSpeed, ySpeed, xScale, yScale);
     }
-    
+
+    show()
+    {
+        //We show our image relative to the correspondant x, y positions and scaling them to the correspondant scale values
+        image(imageFileName, this.x, this.y, this.xScale, this.yScale)
+    }
+
+    update()
+    {
+        this.x = this.x + this.xSpeed;
+        this.y = this.y + this.ySpeed;
+    }
+
+    checkCollision(other, minumumDistance)
+    {
+        //returns true if the distance between "this" object and the "other" object is less than "minumumDistance"
+        return dist(this.x, this.y, other.xPosition, this.yPosition) < minumumDistance;
+    }
+
+
+
 }
