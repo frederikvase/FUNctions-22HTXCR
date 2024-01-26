d939f1e390e9953e3f571a3aea2a69f65aec101e
Culf-B <49363745+Culf-B@users.noreply.github.com>
2024-01-26 13:50:54 +0100

    Fixed indention

--- gameManager.js
+++ gameManager.js
@@ -27,28 +27,28 @@ class GameManager
     }
 
 
-        // Use this function to track the increase in bullet damage from power-ups
-        damageIncrease()
-        {
-    
-        }
-
-        // Use this function to track the increase in attack speed from power-ups
-        attackSpeedIncrease()
-        {
-
-        }
-
-        // Use this function to track the increase in how many enemies bullets can penetrate acquired from power-ups
-        bulletPenetrationIncrease()
-        {
-
-        }
-
-        // Use this function to track the increase in bullet capacity between reloads from power-ups
-        ammoCapacityIncrease()
-        {
-
-        }
+    // Use this function to track the increase in bullet damage from power-ups
+    damageIncrease()
+    {
 
     }
+
+    // Use this function to track the increase in attack speed from power-ups
+    attackSpeedIncrease()
+    {
+
+    }
+
+    // Use this function to track the increase in how many enemies bullets can penetrate acquired from power-ups
+    bulletPenetrationIncrease()
+    {
+
+    }
+
+    // Use this function to track the increase in bullet capacity between reloads from power-ups
+    ammoCapacityIncrease()
+    {
+
+    }
+
+}
