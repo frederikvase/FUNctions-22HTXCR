7366cdb7ec4d423747a858c480e85695506700e5
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2024-02-05 14:55:29 +0100

    Explosion v3.0
    Comments and is ready to use, but without sound and the particals are when spawned but the 'resizing' is to top left corner

--- Explosion.js
+++ Explosion.js
@@ -1,8 +1,8 @@
 class Explosion 
 {
-    // This class takes an x,y coordinate for where the explosion is to happen, 
-    // the amount of particles, the particle movement speed, the duration of the explosion
-    // and start min- and maxSize of the particles
+    // This class takes an x, y coordinate for where the explosion is to happen, 
+    // the amount of particles, the particle movement speed, the dissapering speed of the particles,
+    // start min- and maxSize of the particles
     constructor (x, y, amountOfParticle, particleSpeed, disappearSpeed, sizeParticleMin, sizeParticleMax) 
     { 
         this.x = x;
@@ -19,6 +19,8 @@ class Explosion
         this.makeParticles(amountOfParticle);
     }
 
+    // Private
+    // Makes an array of the particals in the explosion
     makeParticles(amountToMake) 
     {
         for (let i = 0; i < amountToMake; i = i + 1)
@@ -27,14 +29,16 @@ class Explosion
         }
     }
 
-    // Run to make the explosion move
+    // Public
+    // Run in draw to make the explosion move
     explode() 
     { 
         for (let i = 0; i < this.particlesArr.length; i = i + 1)
         {
-            this.particlesArr[i].drawSprite();
-            this.particlesArr[i].move();
+            this.particlesArr[i].drawSprite();  // Draws the explosion particals
+            this.particlesArr[i].move();        // Moves the explosion particals
 
+            // If the partical has gotten a size below or equal to zero remove the partical
             if (this.particlesArr[i].disappear(this.disappearSpeed))
             {
                 this.particlesArr.splice(i,1);
@@ -43,7 +47,8 @@ class Explosion
         }
     }
 
-    // Returns wheter the explosion is done
+    // Public
+    // Returns whether the explosion is done, so that it can be removed
     explosionIsDone() 
     {
         if (this.particlesArr.length == 0)
@@ -60,7 +65,7 @@ class Explosion
 // Private
 class Particle extends Entity 
 {
-    // This class takes an x pos, y pos, size and speed
+    // This class takes an x pos, y pos, size minium, size maximum and speed of the particle
     constructor (x, y, sizeMin, sizeMax, speed) 
     {
         let angle = random(0,360);
@@ -70,28 +75,30 @@ class Particle extends Entity
 
         let size = random(sizeMin, sizeMax)
 
-        let xInput = x - size;
-        let yInput = y - size;
+        console.log(size);
+        let xInput = x;
 
-        super("EksplosionGit\\FUNctions-22HTXCR\\assets\\whitestar.png", xInput, yInput, xSpeed, ySpeed, size, size)
+        super("smallstar.png", x, y, xSpeed, ySpeed, size, size) // The arguments extended from Entity class
 
         this.size = [size, size];
+
+        this.setOrigo((11 * this.size[0]) / 2, (11 * this.size[1]) / 2);
     }
 
     // Makes the particle smaller
     disappear(amountToScale) 
     {
         if (this.size[0] > 0 && this.size[1] > 0)
-        {
+         {
             this.size = [this.size[0] + amountToScale, this.size[1] + amountToScale];
 
             this.setScale(this.size[0], this.size[1]);
             
-            return false;
+            return false; // The particle hasn't disappeared
         } 
         else
         {
-            return true;
+            return true; // The particle has disappeared
         }
     }
 }
