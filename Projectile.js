aed875ffd6abeab7217771591beccbb5fdab486f
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2024-02-19 14:09:28 +0100

    FIX
    LETS GOOO

--- Projectile.js
+++ Projectile.js
@@ -6,33 +6,38 @@ class Projectile extends Entity
         super(imageFileName_, x_, y_, xSpeed_, y_Speed_, x_Scale_, y_Scale_);
     }
 
+    moveProj()
+    {
+        this.x = this.x + this.xSpeed;
+        this.y = this.y + this.ySpeed;
+    }
+
     checkCollision(other, minumumDistance)
     {
         //returns true if the distance between "this" object and the "other" object is less than "minumumDistance".
-        return dist(this.x_, this.y_, other.x_, other.y_) < minumumDistance;
+        return dist(this.x, this.y, other.x, other.y) < minumumDistance;
     }
 
     edgeCollision()
     {
         //if the projectile object reaches out of bounds, it should return true.
-        return this.x_ > width || this.x_ < 0 || this.y_ < 0 || this.y_ > height;
+        return (this.x > width || this.x < 0 || this.y < 0 || this.y > height);
     }
     
     checkDistance(other)
     {
         //returns the distance of this and other
-        return dist(this.x_, this.y_, other.x_, other.y_);
+        return dist(this.x, this.y, other.x, other.y);
     }
 
     homing(other, my_HomingSpeed=3)
     {
         //first find the angle between this and other
-        let angle = atan2(other.y_ - this.y_, other.x_ - this.x_);
+        let angle = atan2(other.y - this.y, other.x - this.x);
         let homingSpeed = my_HomingSpeed;
 
         //sets the speed to match the direction of other akumala akumala
         this.x_Speed = homingSpeed * cos(angle);
         this.y_Speed = homingSpeed * sin(angle);
     }
-
 }
