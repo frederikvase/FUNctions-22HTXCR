562bb14f09bc89c2ee850ded3b88d064d85e46c2
IdaH22R <143402890+IdaH22R@users.noreply.github.com>
2024-03-12 13:02:18 +0100

    flere shtjerneskud

--- Sky.js
+++ Sky.js
@@ -5,7 +5,7 @@ class Sky
         this.stars = [];
         this.shootingStars=[];
 
-        for (let i = 0; i<5; i=i+1)
+        for (let i = 0; i<10; i=i+1)
         {
          this.shootingStars[i]= new Star();
         }
